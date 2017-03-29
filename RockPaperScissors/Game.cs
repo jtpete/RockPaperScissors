e8a68e0cbbe4fe7b8a22936864e857d9f5d01229
jtpete <pastor.jpete@gmail.com>
2017-03-29 11:14:57 -0500

    updated program file for spelling error

--- RockPaperScissors/Game.cs
+++ RockPaperScissors/Game.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PaperRockSissors
+namespace RockPaperScissors
 {
     class Game
     {
--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -10,6 +10,22 @@ namespace RockPaperScissors
     {
         static void Main(string[] args)
         {
+            string response;
+            do
+            {
+                Console.WriteLine("\nWould you like to begin a game...yes or no?\n");
+                response = Console.ReadLine().ToLower();
+                if (response == "yes")
+                {
+                    Game myGame = new Game();
+                    myGame.StartGame();
+                    if (!myGame.HaveWinner() && response == "yes")
+                    {
+                        myGame.PlayGame();
+                    }
+                    myGame.FinishGame();
+                }
+            } while (response == "yes");
         }
     }
 }
